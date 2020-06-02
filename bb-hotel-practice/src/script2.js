d75fec3e850a935f4063a607ee33293ba8a6a507
Steven Chan <richlife1220@gmail.com>
2020-06-02 21:16:48 +0800

    add web page

---
+++ bb-hotel-practice/src/script2.js
@@ -0,0 +1,19 @@
+// NavBar Toggle Class
+$(document).ready(function () {
+    var navMenu = $('.nav-menu')
+    var navContainer = $('.nav-container')
+
+    navMenu.click(function () {
+
+        navContainer.toggleClass('nav-show-menu');
+    });
+
+
+
+
+
+
+
+
+
+})
